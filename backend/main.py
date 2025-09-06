61c73853ba65fe9761b995ed254e66a3bb01908c
dodoser34 <fgydvg@gmail.com>
2025-09-06 20:52:10 +0500

    добавления email-validator==2.3.0

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, Depends
 from backend import auth, db
 from backend.auth import get_current_user
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Messenger Auth with MySQL (no ORM)")
+backend  = FastAPI()
 
-app.include_router(auth.router, prefix="/api/auth")
+backend.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.on_event("startup")
+backend = FastAPI(title="Messenger Auth with MySQL (no ORM)")
+
+backend.include_router(auth.router, prefix="/api/auth")
+
+@backend.on_event("startup")
 def on_startup():
     db.init_db()
 
-@app.get("/api/users/me")
+@backend.get("/api/users/me")
 async def read_me(user=Depends(get_current_user)):
     return user
