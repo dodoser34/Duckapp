b082acdb864e86564c12ba24f4382a53226b901b
dodoser34 <fgydvg@gmail.com>
2025-09-06 21:13:28 +0500

    добавления python-multipart==0.0.12 удаления  pydantic-core==2.39.0 исправления main.py в  backend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
+# main.py
 from fastapi import FastAPI, Depends
 from backend import auth, db
 from backend.auth import get_current_user
 from fastapi.middleware.cors import CORSMiddleware
 
-backend  = FastAPI()
+app = FastAPI(title="Messenger Auth with MySQL (no ORM)")
 
-backend.add_middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
@@ -13,14 +14,12 @@ backend.add_middleware(
     allow_headers=["*"],
 )
 
-backend = FastAPI(title="Messenger Auth with MySQL (no ORM)")
+app.include_router(auth.router, prefix="/api/auth")
 
-backend.include_router(auth.router, prefix="/api/auth")
-
-@backend.on_event("startup")
+@app.on_event("startup")
 def on_startup():
     db.init_db()
 
-@backend.get("/api/users/me")
+@app.get("/api/users/me")
 async def read_me(user=Depends(get_current_user)):
     return user
